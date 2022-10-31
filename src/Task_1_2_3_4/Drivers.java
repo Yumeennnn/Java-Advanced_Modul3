cf3d2d22b129cb1a34029531e736d6d3b757f9c4
Yummm292 <ahmadrizki0209@gmail.com>
2022-10-31 07:12:27 +0700

    Modul3 : Change Variable Name At Main Class

--- src/Task_1_2_3_4/Drivers.java
+++ src/Task_1_2_3_4/Drivers.java
@@ -3,20 +3,23 @@ package Task_1_2_3_4;
 import java.util.Scanner;
 
 public class Drivers {
-    static int a, b, c;
+    static int Panjang, Lebar, Tinggi;
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Balok result = new Balok();
         Perhitungan p = new Perhitungan();
 
-        System.out.print("Masukkan Panjang : "); a = input.nextInt();
-        result.setPanjang(a);
+        System.out.print("Masukkan Panjang : ");
+        Panjang = input.nextInt();
+        result.setPanjang(Panjang);
 
-        System.out.print("Masukkan Lebar   : "); b = input.nextInt();
-        result.setLebar(b);
+        System.out.print("Masukkan Lebar   : ");
+        Lebar = input.nextInt();
+        result.setLebar(Lebar);
 
-        System.out.print("Masukkan Tinggi  : "); c = input.nextInt();
-        result.setTinggi(c);
+        System.out.print("Masukkan Tinggi  : ");
+        Tinggi = input.nextInt();
+        result.setTinggi(Tinggi);
         result.Hasil();
         System.out.println("Balok Is " +result.isCubes(p.Luas(result)));
     }
